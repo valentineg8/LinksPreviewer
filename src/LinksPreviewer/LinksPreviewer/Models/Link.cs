308c2a87c9d2bdec8c7845474b2da432abb48698
Lorenzo Araujo <Lorenzoaraujo12@gmail.com>
2020-05-05 22:39:45 -0400

    Project Added

---
+++ src/LinksPreviewer/LinksPreviewer/Models/Link.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace LinksPreviewer.Models
+{
+    public class Link : INotifyPropertyChanged
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string URL { get; set; }
+        public bool Found {
+            get
+            {
+                return !string.IsNullOrEmpty(Title) || !string.IsNullOrEmpty(Description) || !string.IsNullOrEmpty(Image);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ src/LinksPreviewer/LinksPreviewer/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using LinksPreviewer.Models;
+using Prism.Mvvm;
+
+namespace LinksPreviewer.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INotifyPropertyChanged
+    {
+        public ObservableCollection<Link> Links { get; set; }
+
+        public Link EditorLink { get; set; }
+        public MainPageViewModel()
+        {
+        }
+    }
+}
